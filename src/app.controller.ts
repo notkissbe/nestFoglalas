fe3404862845a8648273b788077f48ce476e4f51
notkissbe <bencetel1@gmail.com>
2024-10-14 13:41:56 +0200

    kesz

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,11 +1,12 @@
 import { Body, Controller, Get, Post, Redirect, Render, Res } from '@nestjs/common';
 import { AppService } from './app.service';
 import { Foglalas } from './foglalas.dto';
+import { Response } from 'express';
 
 
 @Controller()
 export class AppController {
-  constructor(private readonly appService: AppService) {}
+  constructor(private readonly appService: AppService) { }
 
   @Get()
   @Redirect('foglalas')
@@ -17,30 +18,56 @@ export class AppController {
 
   @Get("foglalas")
   @Render("urlap")
-  getFoglalas(){
-    return{
-      errors:[],
-      foglalas:[]
+  getFoglalas() {
+    return {
+      errors: [],
+      data: []
     }
 
   }
   @Get("siker")
   @Render("siker")
-  getSiker(){
-    return{
+  getSiker() {
+    return {
     }
 
   }
 
   @Post("foglalas")
   postFoglalas(
-    @Body() Foglalas:Foglalas,
-    @Res() response:Response
-  ){
+    @Body() Foglalas: Foglalas,
+    @Res() response: Response
+  ) {
     let errors = [];
 
-    console.log()
-    return Response.redirect("/siker")
+    if (!Foglalas.nev || !Foglalas.email || !Foglalas.datum || !Foglalas.ido || !Foglalas.vendegszam) {
+      errors.push("Minden mezőt ki kell tölteni!");
+    }
+
+    if(!/^[\w\.]+@[\w-\.]+$/.test(Foglalas.email)){
+      errors.push("Nem megfelelő email formátum!");
+    }
+
+    if(Date.parse(`${Foglalas.datum} ${Foglalas.ido}`) < Date.now()){
+      errors.push("Múltbéli időpont!");
+    }
+
+    if(Foglalas.vendegszam < 1 || Foglalas.vendegszam > 10){
+      errors.push("Nem megfelelő a vendégek száma!");
+    }
+
+    if (errors.length > 0) {
+      response.render("urlap", {
+        data: Foglalas,
+        errors: errors
+      })
+      return errors;
+    }
+
+    console.log(Foglalas);
+    
+
+    return response.redirect('/siker')
   }
 
 
